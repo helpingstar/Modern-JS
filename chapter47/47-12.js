47b7de80fba533da12fb0f46bbc2f7ff222ea096
helpingstar <iamhelpingstar@gmail.com>
2023-11-13 16:41:32 +0900

    add ch47

---
+++ chapter47/47-12.js
@@ -0,0 +1,19 @@
+// 47-12
+
+const foo = () => {
+    throw Error('foo에서 발생한 에러'); // ④
+};
+
+const bar = () => {
+    foo(); // ③
+};
+
+const baz = () => {
+    bar(); // ②
+};
+
+try {
+    baz(); // ①
+} catch (err) {
+    console.error(err);
+}
